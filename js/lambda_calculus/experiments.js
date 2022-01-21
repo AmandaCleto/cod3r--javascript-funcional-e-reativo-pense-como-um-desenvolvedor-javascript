2b977b952e8a80fb88a7833a7496ab32f452a7d6
Amanda Cleto <amanda.clet@gmail.com>
2022-01-20 21:21:12 -0300

    last experiments with lambda calculus logic

--- js/lambda_calculus/experiments.js
+++ js/lambda_calculus/experiments.js
@@ -1,54 +1,46 @@
+Number.prototype.log = function() { console.log(this) };
+Function.prototype.log = function() { console.log(this.toString()) };
+
 let results
 
 const IDENTITY = a => a
-results = IDENTITY(3)
-results
+IDENTITY(3).log()
 
-results = IDENTITY(IDENTITY)
-results
+IDENTITY(IDENTITY).log()
 
 const SELF = f => f(f)
-results = SELF(IDENTITY)
-results
+SELF(IDENTITY).log()
 
 const FIRST = a => _ => a
-results = FIRST(2)(4)
-results
+FIRST(2)(4).log()
 
 const LAST = a => b => b
-results = LAST(2)(4)
-results
+LAST(2)(4).log()
 
 const FLIP = f => a => b => f(b)(a)
-results = FLIP(LAST)(2)(4)
-results
-results = FLIP(FIRST)(2)(4)
-results
+FLIP(LAST)(2)(4).log()
+FLIP(FIRST)(2)(4).log()
 
 const NEWLAST = a => b => FLIP(FIRST)(a)(b)
-results = NEWLAST(2)(4)
-results
+NEWLAST(2)(4).log()
 
 //BOOLEAN TRUE or FALSE
 //expression ? true : false
 //TRUE ? <FIRST> : LAST
 //FALSE ? LAST : <FIRST>
-
 const TRUE = FIRST
 const FALSE = LAST
 
-TRUE.inspect = () => 'True = (FIRST)'
-FALSE.inspect = () => 'False = (LAST)'
+TRUE.toString = () => 'True = (FIRST)'
+FALSE.toString = () => 'False = (LAST)'
 
 TRUE
 FALSE
 
 //NOT - unity operator
 const NOT = a => a(FALSE)(TRUE)
-results = NOT(FALSE)
-results
-results = NOT(TRUE)
-results
+NOT(FALSE).log()
+NOT(TRUE).log()
 
 //AND - binary operator
 //TRUE && TRUE == TRUE
@@ -56,36 +48,24 @@ results
 //FALSE && TRUE == FALSE
 //FALSE && FALSE == FALSE
 const AND = a => b => a(b)(FALSE)
-results = AND(FALSE)(FALSE)
-results
-results = AND(FALSE)(TRUE)
-results
-results = AND(TRUE)(FALSE)
-results
-results = AND(TRUE)(TRUE)
-results
+AND(FALSE)(FALSE).log()
+AND(FALSE)(TRUE).log()
+AND(TRUE)(FALSE).log()
+AND(TRUE)(TRUE).log()
 
 //outra implementação do AND
 const AND2 = a => b => a(b)(a)
-results = AND2(FALSE)(FALSE)
-results
-results = AND2(FALSE)(TRUE)
-results
-results = AND2(TRUE)(FALSE)
-results
-results = AND2(TRUE)(TRUE)
-results
+AND2(FALSE)(FALSE).log()
+AND2(FALSE)(TRUE).log()
+AND2(TRUE)(FALSE).log()
+AND2(TRUE)(TRUE).log()
 
 //outra implementação do AND
 const AND3 = a => b => b(a)(b)
-results = AND3(FALSE)(FALSE)
-results
-results = AND3(FALSE)(TRUE)
-results
-results = AND3(TRUE)(FALSE)
-results
-results = AND3(TRUE)(TRUE)
-results
+AND3(FALSE)(FALSE).log()
+AND3(FALSE)(TRUE).log()
+AND3(TRUE)(FALSE).log()
+AND3(TRUE)(TRUE).log()
 
 //OR - binary operator
 //TRUE || TRUE == TRUE
@@ -93,36 +73,34 @@ results
 //FALSE || TRUE == TRUE
 //FALSE || FALSE == FALSE
 const OR = a => b => a(TRUE)(b)
-results = OR(FALSE)(FALSE)
-results
-results = OR(FALSE)(TRUE)
-results
-results = OR(TRUE)(FALSE)
-results
-results = OR(TRUE)(TRUE)
-results
+OR(FALSE)(FALSE).log()
+OR(FALSE)(TRUE).log()
+OR(TRUE)(FALSE).log()
+OR(TRUE)(TRUE).log()
 
 const OR2 = a => b => a(a)(b)
-results = OR2(FALSE)(FALSE)
-results
-results = OR2(FALSE)(TRUE)
-results
-results = OR2(TRUE)(FALSE)
-results
-results = OR2(TRUE)(TRUE)
-results
+OR2(FALSE)(FALSE).log()
+OR2(FALSE)(TRUE).log()
+OR2(TRUE)(FALSE).log()
+OR2(TRUE)(TRUE).log()
 
 //EQ = === - binary operator
 //TRUE === TRUE = TRUE
 //FALSE === FALSE = TRUE
 //TRUE === FALSE = FALSE
 //FALSE === TRUE = FALSE
-const EQ = a => b => b(b)(FALSE)
-results = EQ(TRUE)(FALSE)
-results
-results = EQ(FALSE)(TRUE)
-results
-results = EQ(TRUE)(TRUE)
-results
-results = EQ(FALSE)(FALSE)
-results
+const EQ = a => b => a(b)(NOT(b))
+EQ(TRUE)(FALSE).log()
+EQ(FALSE)(TRUE).log()
+EQ(TRUE)(TRUE).log()
+EQ(FALSE)(FALSE).log()
+//XOR = === - binary operator
+//TRUE === TRUE = FALSE
+//FALSE === FALSE = FALSE
+//TRUE === FALSE = TRUE
+//FALSE === TRUE = TRUE
+const XOR = a => b => NOT(EQ(a)(b))
+XOR(TRUE)(FALSE).log()
+XOR(FALSE)(TRUE).log()
+XOR(TRUE)(TRUE).log()
+XOR(FALSE)(FALSE).log()
